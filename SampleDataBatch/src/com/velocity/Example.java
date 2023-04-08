2d9c88d75fbce53af0af0b1e34893953c27edd36
Shubham <gshubham9111@gmail.com>
2023-04-08 23:15:50 +0530

    this is java program

--- SampleDataBatch/src/com/velocity/Example.java
+++ SampleDataBatch/src/com/velocity/Example.java
@@ -2,8 +2,8 @@ package com.velocity;
 
 public class Example {
 	public static void main(String[] args) {
-		
-		for(int i=1;i<=5;i++) {
+
+		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 		}
 	}
